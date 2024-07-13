6b9ad3228e2d220d49a91ba689374881f5a6d69f
Adam Astamir <adam.astamir@hotmail.de>
2024-07-13 14:56:54 +0200

    ,

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,7 +10,7 @@ function About() {
             <h1>ABOUT US</h1>
             <p>At Pizza, we believe that great pizza starts with the best ingredients. That's why we use only the finest, freshest ingredients in our recipes. From our hand-tossed dough to our rich, flavorful sauces, every element of our pizzas is crafted with care and precision. We source our toppings from trusted suppliers to ensure that every bite is a burst of deliciousness.
 
-Whether you're a fan of classic Margherita, crave the bold flavors of a Meat Lovers, or prefer a veggie-packed delight, our diverse menu has something for everyone. And if you're feeling adventurous, you can create your own pizza masterpiece with our extensive selection of toppings.</p>
+Whether you're a fan of classic Margherita, crave the bold flavors of a Meat Lovers, or prefer a veggie-packed delight, our diverse menu has something for everyone. And if you're feeling adventurous, you can create your own pizza masterpiece with our extensive selection of toppings</p>
         </div>
     </div>
   )
